8954dc1dad87aeaf7e90053700e63e91f3e5307d
Kamila-Kyncl <kamilka.frolikova@gmail.com>
2025-12-30 14:55:09 +0100

    Určení bulls a cows

--- main.py
+++ main.py
@@ -43,6 +43,44 @@ def validate_tip(tip):
     
     return tip
 
+def resolve(secret, tip):
+    """
+    Vrací počet bulls a cows
+    
+    :param secret: tajné číslo
+    :param tip: tip uživatele
+    """
+    bulls = 0
+    cows = 0
+
+    for i in range(NUMBER_LENGTH):
+        if tip[i] == secret[i]:
+            bulls += 1
+        elif tip[i] in secret:
+            cows += 1
+
+    return bulls, cows
+
+def print_results(bulls, cows):
+    """
+    Vypíše počet bulls a cows
+    
+    :param bulls: počet bulls
+    :param cows: počet cows
+    """
+    bulls_text = "bull"
+    cows_text = "cow"
+
+    if bulls != 1:
+        bulls_text += "s"
+
+    if cows != 1:
+        cows_text += "s"
+
+    print(str(bulls) + " " + bulls_text + ", " + str(cows) + " " + cows_text)
+    
+
+SECRET_NUMBER = create_secret_number()
 
 print("Hi there")
 print("-" * SEPARATOR_LENGTH)
@@ -58,3 +96,7 @@ while True:
         break
     else:
         print(validated_tip + "\n" + "-" * SEPARATOR_LENGTH + "\n")
+
+bulls_cows = resolve(SECRET_NUMBER, validated_tip)
+print_results(bulls_cows[0], bulls_cows[1])
+print(SECRET_NUMBER)
